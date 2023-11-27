6eafebcd65ce876d8f2bc970c9a3dcbebfdeddc8
Konrad Fedorczyk <fedek6@gmail.com>
2023-11-27 18:57:11 +0100

    Add active menu links

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,10 +1,17 @@
 import { Menu as MenuType } from "../config";
-import { Link } from "react-router-dom"
-import { nanoid } from 'nanoid';
+import { NavLink } from "react-router-dom";
+import { nanoid } from "nanoid";
+import tw, { styled } from "twin.macro";
 
 interface Props {
   elements: MenuType[];
 }
+
+const Link = styled.span<{ isActive: boolean }>(({ isActive }) => [
+  tw`block p-1`,
+  isActive ? tw`text-blue-500` : tw`text-gray-600`,
+  tw`hover:text-red-600`
+]);
 
 export const Menu: React.FC<Props> = ({ elements }) => {
   return (
@@ -12,7 +19,11 @@ export const Menu: React.FC<Props> = ({ elements }) => {
       <ul>
         {elements.map((element) => (
           <li key={nanoid()}>
-            <Link to={element.path}>{element.title}</Link>
+            <NavLink to={element.path}>
+              {({ isActive }) => (
+                <Link isActive={isActive}>{element.title}</Link>
+              )}
+            </NavLink>
           </li>
         ))}
       </ul>
